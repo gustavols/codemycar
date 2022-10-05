f1b13ce0267a920d648904a53fef405f1d0a39a6
gustavols <gustavoleandro616@gmail.com>
2022-10-04 22:46:58 -0300

    Feat: Iniciado o projeto codemycar

---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+    Container,
+    WomanSection,
+    LogoContainer,
+    ViewTitleLogin,
+    TitleLogin,
+    ButtonLogin,
+    ViewButtonLogin,
+    TextButtonLogin,
+    ViewTextLogin,
+    ViewLoginGoogle
+    
+} from './styles';
+
+import Logo from "../../assets/images/WomanPhoto.svg";
+import LoginGoogle from "../../assets/images/LoginGoogle.svg";
+import { Button } from "react-native";
+
+export function Login() {
+    return (
+        <Container>
+            <WomanSection>
+                <LogoContainer>
+                    <Logo width="350" height="350" />
+                </LogoContainer>
+            </WomanSection>
+
+            <ViewTitleLogin>
+                <TitleLogin>
+                    Descubra o problema do seu carro
+                </TitleLogin>
+            </ViewTitleLogin>
+
+            <ViewButtonLogin>
+                <ButtonLogin>
+                    <ViewLoginGoogle>
+                        <LoginGoogle width="32" height="32.49"/>
+                    </ViewLoginGoogle>
+                    <ViewTextLogin>
+                        <TextButtonLogin>Entrar com Google</TextButtonLogin>
+                    </ViewTextLogin>
+                </ButtonLogin>
+            </ViewButtonLogin>
+        </Container>
+    );
+}
---
+++ src/screens/Login/styles.ts
@@ -0,0 +1,87 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    background-color: #FFFEFD;
+    height: 100%;
+    width: 100%;
+`;
+
+export const WomanSection = styled.View`
+    height: 40%;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    background-color: #FF4B26;
+`;
+
+export const LogoContainer = styled.View`
+    margin-top: 40%;
+`;
+
+export const ViewTitleLogin = styled.View`
+    display: flex;
+    margin: 0 auto;
+    max-width: 250px;
+    text-align: center;
+    margin-top: 35%;
+   
+`;
+
+export const TitleLogin = styled.Text`
+    font-family: 'Lexend';
+    font-style: normal;
+    font-weight: 800;
+    font-size: 32px;
+    line-height: 40px;
+    color: #585666;
+    text-align: center;
+`;
+
+export const ViewButtonLogin = styled.View`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 6%;
+`;
+
+export const ButtonLogin = styled.TouchableOpacity`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 80%;
+    height: 60px;
+    background: #FAFAFC;
+    border: 1px solid #E3E3E6;
+    border-radius: 5px;
+`;
+
+
+export const ViewLoginGoogle = styled.View`
+    flex-basis: 25%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    border-right: 1px solid #E9E9EB;
+`;
+
+export const ViewTextLogin = styled.View`
+    flex-basis: 75%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`;
+
+
+export const TextButtonLogin = styled.Text`
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 18px;
+    color: #666666;
+`;
+
+
